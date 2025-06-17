167ed4d3076f6636156021d82ac9a40f3894ef3a
Karoki <wanjikukar19@gmail.com>
2025-06-17 21:25:58 +0300

    Initial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Scroll to section smoothly
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute('href'));
+    if (section) {
+      section.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
